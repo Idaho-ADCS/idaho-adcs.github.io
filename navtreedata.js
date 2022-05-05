a1d4443ba639ef17454a28df8e91f4846ea9f40d
Garrett <deepg2w@gmail.com>
2022-05-05 16:09:03 -0700

    second verion

--- navtreedata.js
+++ navtreedata.js
@@ -24,12 +24,45 @@
 */
 var NAVTREE =
 [
-  [ "University of Idaho ADCS", "index.html", ]
+  [ "University of Idaho ADCS", "index.html", [
+    [ "Single Axis Attitude Determination and Control System v1.0", "index.html", [
+      [ "Contents", "index.html#autotoc_md11", null ],
+      [ "Source Code Links", "index.html#autotoc_md12", [
+        [ "Production Code", "index.html#autotoc_md13", null ],
+        [ "Test Code", "index.html#autotoc_md14", null ],
+        [ "Satellite Simulator", "index.html#autotoc_md15", null ]
+      ] ],
+      [ "RTOS", "index.html#autotoc_md16", null ]
+    ] ],
+    [ "ADCSPhotodiodeArray: Multiplexed Photodiode Library", "md_lib__a_d_c_s_photodiode_array__r_e_a_d_m_e.html", null ],
+    [ "DRV10970 Motor Driver", "md_lib__d_r_v10970__r_e_a_d_m_e.html", null ],
+    [ "SparkFun ICM-20948 Arduino Library", "md_lib__i_c_m_20948__r_e_a_d_m_e.html", [
+      [ "Contributing", "md_lib__i_c_m_20948__r_e_a_d_m_e.html#autotoc_md3", null ],
+      [ "Repository Contents", "md_lib__i_c_m_20948__r_e_a_d_m_e.html#autotoc_md4", null ],
+      [ "Documentation", "md_lib__i_c_m_20948__r_e_a_d_m_e.html#autotoc_md5", null ],
+      [ "Products that use this Library", "md_lib__i_c_m_20948__r_e_a_d_m_e.html#autotoc_md6", null ],
+      [ "License Information", "md_lib__i_c_m_20948__r_e_a_d_m_e.html#autotoc_md7", null ]
+    ] ],
+    [ "ZXMB5210 Magnetorquer Driver", "md_lib__z_x_m_b5210__r_e_a_d_m_e.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ]
+    ] ]
+  ] ]
 ];
 
 var NAVTREEINDEX =
 [
-"index.html"
+"_a_d_c_s_photodiode_array_8cpp_source.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
